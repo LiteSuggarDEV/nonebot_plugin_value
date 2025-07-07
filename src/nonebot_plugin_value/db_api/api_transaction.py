7ab26741794e976f4f606fff1c9e747d022ee13c
John Richard <67693593+JohnRichard4096@users.noreply.github.com>
2025-07-07 22:57:46 +0800

    移动API，修改部分Session调用 (#15)

--- src/nonebot_plugin_value/db_api/api_transaction.py
+++ src/nonebot_plugin_value/db_api/api_transaction.py
@@ -1,18 +1,16 @@
-from uuid import UUID
-
 from nonebot_plugin_orm import AsyncSession
 
-from .repository import TransactionRepository
+from ..repository import TransactionRepository
 
 
 async def get_transaction_history(
-    session: AsyncSession, account_id: UUID, limit: int = 100
+    session: AsyncSession, account_id: str, limit: int = 100
 ):
     """获取一个用户的交易记录
 
     Args:
         session (AsyncSession): 异步数据库会话
-        account_id (UUID): 用户UUID
+        account_id (str): 用户UUID(应自行处理)
         limit (int, optional): 数据条数. Defaults to 100.
 
     Returns:
