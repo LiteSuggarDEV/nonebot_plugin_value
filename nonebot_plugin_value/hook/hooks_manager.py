48ea5d1455c32eb5fd6a957c1d651b67c7f103fc
John Richard <windowserror@163.com>
2025-08-03 16:47:05 +0800

    添加依赖注入单元测试，规范化代码。

--- nonebot_plugin_value/hook/hooks_manager.py
+++ nonebot_plugin_value/hook/hooks_manager.py
@@ -1,11 +1,10 @@
 # 事件预处理/后处理钩子
 from collections.abc import Awaitable, Callable
-from typing import Any
 
 from nonebot import logger
 
 from .context import TransactionComplete, TransactionContext
-from .exception import CancelAction
+from .exception import CancelAction, DataUpdate
 from .hooks_type import HooksType
 
 
@@ -13,8 +12,7 @@ class HooksManager:
     __hooks: dict[str, list[Callable[..., Awaitable[None]]]]
     _instance = None
 
-    def __new__(cls, *args: Any, **kwargs: Any):
-        """重写 __new__ 方法实现单例模式"""
+    def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance.__hooks = {}
@@ -38,7 +36,7 @@ class HooksManager:
         self, hook_name: str, hook_func: Callable[..., Awaitable[None]]
     ) -> None:
         """注册一个Hook"""
-        if not HooksType.valid_hooks(hook_name):
+        if hook_name not in HooksType:
             raise ValueError(f"Invalid hook name: {hook_name}")
         self.__hooks.setdefault(hook_name, []).append(hook_func)
 
@@ -51,7 +49,7 @@ class HooksManager:
         async def _run_single_hook(hook: Callable[..., Awaitable[None]]) -> None:
             try:
                 await hook(context)
-            except CancelAction:
+            except CancelAction | DataUpdate:
                 raise
             except Exception:
                 logger.opt(exception=True).error("钩子执行失败")
--- nonebot_plugin_value/hook/hooks_type.py
+++ nonebot_plugin_value/hook/hooks_type.py
@@ -1,19 +1,17 @@
-class HooksType:
-    __pre = "vault_pre_transaction"
-    __post = "vault_post_transaction"
+from enum import Enum
+
+
+class HooksType(Enum, str):
+    PRE = "vault_pre_transaction"
+    POST = "vault_post_transaction"
 
     @classmethod
     def pre(cls) -> str:
-        return cls.__pre
+        return cls.PRE
 
     @classmethod
     def post(cls) -> str:
-        return cls.__post
-
-    @classmethod
-    def valid_hooks(cls, hook_name: str) -> bool:
-        return hook_name in [cls.__pre, cls.__post]
-
+        return cls.POST
     @classmethod
     def methods(cls) -> list[str]:
-        return [cls.__pre, cls.__post]
+        return [hook.value for hook in cls]
